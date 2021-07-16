8b9f23a18835b347063c8974cd0a2b0bdb23e27c
Mitchell Rodríguez <mitrodle@gmail.com>
2021-07-15 23:15:42 -0500

    [Add]: API Demo

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -1,20 +1,25 @@
 import Link from 'next/link'
 import styles from '../../styles/Timeline.module.css'
 
-export default function Timeline() {
+export default function Timeline({userName}) {
     return (
      <>
-         <h1 className={styles.title}>This is a timeline</h1>
+         <h1 className={styles.title}>This is a timeline of {userName}</h1>
          <Link href='/'>Go home</Link>
-{/* 
-         <style jsx> {
-             `
-             h1 {
-                 font-size: 36px;
-                 color: red;
-             }
-             `
-         } </style> */}
      </>   
     )
+}
+
+// Executes on server side BEFORE render
+// Only works in "page" components
+Timeline.getInitialProps = () => {
+    // return { userName: '@mishrole'}
+    return fetch('http://localhost:3000/api/hello')
+        .then(res => res.json())
+        .then( (response) => {
+            console.log(response);
+            // const {userName} = response
+            // return {userName}
+            return response
+        });
 }
